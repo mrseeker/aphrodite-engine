11a441af878df96e759c8829a654d7dd53fb4249
mrseeker <1099127+mrseeker@users.noreply.github.com>
2023-11-30 15:30:55 +0100

    Missing API key

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,6 +7,7 @@ CMD="python -u -m aphrodite.endpoints.openai.api_server \
              --host 0.0.0.0 \
              --port 7860 \
              --model $MODEL_NAME \
+             --api-keys $API_KEY \
              --tensor-parallel-size $NUM_GPUS \
              --dtype $DATATYPE"
 
